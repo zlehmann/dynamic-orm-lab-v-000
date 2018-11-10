35c34c93e821e299985de9bcfec6bbe855f4f056
Zachary Lehmann <zlehmann@gmail.com>
2018-11-10 17:42:19 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -34,4 +34,8 @@ class InteractiveRecord
   def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
+
+  def values_for_insert
+    self.class.col_names_for_insert
+  end
 end
